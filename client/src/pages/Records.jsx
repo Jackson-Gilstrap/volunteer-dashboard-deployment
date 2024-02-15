67ff1da0aca842d077b4410166121bc1085890bc
Jackson-Gilstrap <Jacksoncg730@gmail.com>
2024-02-15 13:47:18 -0500

    added admin auth to records screen

--- client/src/pages/Records.jsx
+++ client/src/pages/Records.jsx
@@ -1,22 +1,56 @@
 import ShowVolunteers from "../components/ShowVolunteers";
 import ShowVolunteerWorkSession from "../components/ShowVolunteerWorkSession";
+import { useState } from "react";
 import "../styles/main.css";
 const Records = () => {
+  const [accessCode, setAccessCode] = useState("");
+  const [authStatus, setAuthStatus] = useState(false);
+
+  const adminCode = "sc2024";
+
+  const handleChange = (e) => {
+    setAccessCode(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (accessCode.toLowerCase() === adminCode.toLowerCase()) {
+      setAuthStatus(true);
+    } else {
+      alert("Access denied wrong code");
+    }
+    setAccessCode("");
+  };
+
   return (
     <>
       <div
         style={{
           display: "flex",
           flexDirection: "column",
-          // position: "relative",
-          // left: "10%",
           margin: "auto",
         }}
       >
-        <div className="records_flex">
-          <ShowVolunteers />
-          <ShowVolunteerWorkSession />
-        </div>
+        {authStatus ? (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="authCode">
+              Admin Code:{" "}
+              <input
+                type="password"
+                name="authCode"
+                value={accessCode}
+                onChange={handleChange}
+              />
+            </label>
+
+            <button type="submit">Submit</button>
+          </form>
+        ) : (
+          <div className="records_flex">
+            <ShowVolunteers />
+            <ShowVolunteerWorkSession />
+          </div>
+        )}
       </div>
     </>
   );
