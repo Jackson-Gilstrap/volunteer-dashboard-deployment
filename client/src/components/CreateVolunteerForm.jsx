c9cc857ceb294b8a1a2f03a73ef48fbacf055886
Jackson-Gilstrap <Jacksoncg730@gmail.com>
2024-02-15 13:57:15 -0500

    added admin code to create volunteer

--- client/src/components/CreateVolunteerForm.jsx
+++ client/src/components/CreateVolunteerForm.jsx
@@ -32,6 +32,24 @@ const validate = (values) => {
 //form component
 const CreateVolunteerForm = () => {
   const [successMessage, setSuccessMessage] = useState("");
+  const [accessCode, setAccessCode] = useState("");
+  const [authStatus, setAuthStatus] = useState(false);
+
+  const adminCode = "sc2024";
+
+  const handleChangeAdmin = (e) => {
+    setAccessCode(e.target.value);
+  };
+
+  const handleSubmitAdmin = (e) => {
+    e.preventDefault();
+    if (accessCode.toLowerCase() === adminCode.toLowerCase()) {
+      setAuthStatus(true);
+    } else {
+      alert("Access denied wrong code");
+    }
+    setAccessCode("");
+  };
   const formik = useFormik({
     initialValues: {
       volunteer_code: "",
@@ -68,56 +86,72 @@ const CreateVolunteerForm = () => {
   });
   return (
     <>
-      <form onSubmit={formik.handleSubmit}>
-        <div className="input-container" id="volunteer_code-container">
-          <label htmlFor="volunteer_code">Volunteer Code</label>
-          <input
-            id="volunteer_code"
-            name="volunteer_code"
-            placeholder="123"
-            type="text"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.volunteer_code}
-          />
-          {formik.touched.volunteer_code && formik.errors.volunteer_code ? (
-            <span>{formik.errors.volunteer_code}</span>
-          ) : null}
-        </div>
-        <div className="input-container" id="first_name-container">
-          <label htmlFor="first_name">First Name</label>
-          <input
-            id="first_name"
-            name="first_name"
-            placeholder="John"
-            type="text"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.first_name}
-          />
-          {formik.touched.first_name && formik.errors.first_name ? (
-            <span>{formik.errors.first_name}</span>
-          ) : null}
-        </div>
-        <div className="input-container" id="last_name-container">
-          <label htmlFor="last_name">Last Name</label>
-          <input
-            id="last_name"
-            name="last_name"
-            placeholder="123"
-            type="text"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.last_name}
-          />
-          {formik.touched.last_name && formik.errors.last_name ? (
-            <span>{formik.errors.last_name}</span>
-          ) : null}
-        </div>
-        <button type="submit" name="submit">
-          Save
-        </button>
-      </form>
+      {authStatus ? (
+        <form onSubmit={formik.handleSubmit}>
+          <div className="input-container" id="volunteer_code-container">
+            <label htmlFor="volunteer_code">Volunteer Code</label>
+            <input
+              id="volunteer_code"
+              name="volunteer_code"
+              placeholder="123"
+              type="text"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.volunteer_code}
+            />
+            {formik.touched.volunteer_code && formik.errors.volunteer_code ? (
+              <span>{formik.errors.volunteer_code}</span>
+            ) : null}
+          </div>
+          <div className="input-container" id="first_name-container">
+            <label htmlFor="first_name">First Name</label>
+            <input
+              id="first_name"
+              name="first_name"
+              placeholder="John"
+              type="text"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.first_name}
+            />
+            {formik.touched.first_name && formik.errors.first_name ? (
+              <span>{formik.errors.first_name}</span>
+            ) : null}
+          </div>
+          <div className="input-container" id="last_name-container">
+            <label htmlFor="last_name">Last Name</label>
+            <input
+              id="last_name"
+              name="last_name"
+              placeholder="123"
+              type="text"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.last_name}
+            />
+            {formik.touched.last_name && formik.errors.last_name ? (
+              <span>{formik.errors.last_name}</span>
+            ) : null}
+          </div>
+          <button type="submit" name="submit">
+            Save
+          </button>
+        </form>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="authCode">
+            Admin Code:{" "}
+            <input
+              type="password"
+              name="authCode"
+              value={accessCode}
+              onChange={handleChange}
+            />
+          </label>
+
+          <button type="submit">Submit</button>
+        </form>
+      )}
       {successMessage && (
         <p style={{ textAlign: "center", color: "limegreen" }}>
           {successMessage}
